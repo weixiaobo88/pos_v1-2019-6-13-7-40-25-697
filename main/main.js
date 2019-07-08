8993365ba65c18216e4f20f875a811591ace1c4f
Zhaoyu <weixiaobo88@gmail.com>
2019-07-08 11:11:30 +0800

    finish count tags

--- main/main.js
+++ main/main.js
@@ -16,11 +16,51 @@ ${receiptDetail}
   console.log(result);
 }
 
-function countTags(tags) {}
+function countTags(tags) {
+  let result = [];
+  tags.forEach(tag => {
+    const matchedIndex = findMatchedIndex(result, tag);
+    if (matchedIndex > -1) {
+      const matched = result[matchedIndex];
+      result[matchedIndex] = {
+        barcode: matched.barcode,
+        count: matched.count + 1
+      };
+    } else {
+      result.push({ barcode: tag, count: 1 });
+    }
+  });
+  result = handleSpecial(result);
+  console.info(result);
+  return result;
+}
+
+function findMatchedIndex(collection, barcode) {
+  return collection.findIndex(item => item.barcode === barcode);
+}
+
+function handleSpecial(countedTags) {
+  return countedTags.map(tag => {
+    const barcode = tag.barcode;
+    if (hasDash(barcode)) {
+      return countDashTag(barcode);
+    }
+    return tag;
+  });
+}
+
+function hasDash(barcode) {
+  return barcode.indexOf("-") > -1;
+}
+
+function countDashTag(barcode) {
+  const splited = barcode.split("-");
+  return { barcode: splited[0], count: parseFloat(splited[1]) };
+}
 
 function getItems(countedTags, allItems) {}
 
-function getReceiptDetail() {
+function getReceiptDetail(countedItems) {
   return `名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)`;
